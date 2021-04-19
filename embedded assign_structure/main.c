5b4a96ba5b840ae63de0031b5c4d0bda1b2ce1d5
Omar ElNajjar <omarel.najjar19@gmail.com>
2021-04-19 23:09:06 +0200

    adding the file of the structure

---
+++ embedded assign_structure/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct student
+{
+    char name[50];
+    int student_id;
+    int day, month, year;
+    int score;
+    struct student*next;
+};
+int main()
+{
+    printf("Welcome to our Embedded assignment\n");
+    struct student student1;
+    printf("enter the name of the student\n");
+    scanf("%s", &student1.name);
+    printf("enter the student_ID\n");
+    scanf("%d", &student1.student_id);
+    printf("enter the birthdate\n");
+    printf("enter day / month/year\n");
+    scanf("%d%d%d", &student1.day, &student1.month, &student1.year);
+    printf("enter the previous year score \n");
+    scanf("%d", &student1.score);
+    return 0;
+}
